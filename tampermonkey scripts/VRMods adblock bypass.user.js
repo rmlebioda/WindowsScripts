93ff023f95511c78ae02b75ecc8c084f8e7dc38c
rmlebioda <lebioda.rafal@gmail.com>
2020-01-04 20:08:27 +0100

    new tampermonkey vrchat script

---
+++ tampermonkey scripts/VRMods adblock bypass.user.js
@@ -0,0 +1,14 @@
+// ==UserScript==
+// @name         VRMods adblock bypass
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @match        https://vrcmods.com/
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    var re = /vrcmods\.com\/download\/(?=\w)/;
+    window.location = document.URL.replace(re, "vrmods.com/download/direct/")
+})();
