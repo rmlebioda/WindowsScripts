fa19bc594f15f213f9f714c09a76aa319074edf0
rmlebioda <lebioda.rafal@gmail.com>
2020-01-04 20:25:34 +0100

    fix

--- tampermonkey scripts/VRMods adblock bypass.user.js
+++ tampermonkey scripts/VRMods adblock bypass.user.js
@@ -4,11 +4,11 @@
 // @version      0.1
 // @description  try to take over the world!
 // @author       You
-// @match        *://vrcmods.com/*
+// @match        *://vrcmods.com/download/*
 // @grant        none
 // ==/UserScript==
 
 (function() {
     var re = /vrcmods\.com\/download\/(?=\d)/;
-    window.location = document.URL.replace(re, "vrcmods.com/download/direct/")
+    window.open(document.URL.replace(re, "vrcmods.com/download/direct/"))
 })();
