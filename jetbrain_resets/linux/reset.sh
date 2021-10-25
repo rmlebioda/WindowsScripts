9d84321e979f14d3e6a5d869075bee992ad09784
rmlebioda <lebioda.rafal@gmail.com>
2021-10-25 21:11:15 +0200

    Added jetbrains trial reset scripts

---
+++ jetbrain_resets/linux/reset.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#!/bin/sh
+# reset jetbrains ide evals
+
+OS_NAME=$(uname -s)
+JB_PRODUCTS="IntelliJIdea CLion PhpStorm GoLand PyCharm WebStorm Rider DataGrip RubyMine AppCode"
+
+for PRD in $JB_PRODUCTS; do
+	rm -rf ~/.java/.userPrefs/prefs.xml
+	rm -rf ~/.java/.userPrefs/jetbrains/prefs.xml
+	rm -rf ~/.config/JetBrains/${PRD}*/eval/
+	rm -rf ~/.config/JetBrains/${PRD}*/options/other.xml
+	# those 2 below were necessary for dotCover and dotTrace resets
+	rm -rf ~/.java/.userPrefs/jetbrains/dc/
+	rm -rf ~/.java/.userPrefs/jetbrains/dpn/
+done
---
+++ jetbrain_resets/macos/reset.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+#https://github.com/PythonicNinja/jetbrains-reset-trial-mac-osx/blob/master/runme.sh
+#https://gist.github.com/Karasiq/f404c56963cd9fd93f5fc0fbccff6776
+
+for product in IntelliJIdea WebStorm DataGrip PhpStorm CLion PyCharm GoLand RubyMine Rider; do
+  echo "Closing $product"
+  ps aux | grep -i MacOs/$product | cut -d " " -f 5 | xargs kill -9
+
+  echo "Resetting trial period for $product"
+
+  echo "removing evaluation key..."
+  rm -rf ~/Library/Preferences/$product*/eval
+
+  # Above path not working on latest version. Fixed below
+  rm -rf ~/Library/Application\ Support/JetBrains/$product*/eval
+
+  echo "removing all evlsprt properties in options.xml..."
+  sed -i '' '/evlsprt/d' ~/Library/Preferences/$product*/options/other.xml
+
+  # Above path not working on latest version. Fixed below
+  sed -i '' '/evlsprt/d' ~/Library/Application\ Support/JetBrains/$product*/options/other.xml
+
+  echo
+done
+
+echo "removing additional plist files..."
+rm -f ~/Library/Preferences/com.apple.java.util.prefs.plist
+rm -f ~/Library/Preferences/com.jetbrains.*.plist
+rm -f ~/Library/Preferences/jetbrains.*.*.plist
+
+echo "restarting cfprefsd"
+killall cfprefsd
+
+echo
+echo "Finished" 
